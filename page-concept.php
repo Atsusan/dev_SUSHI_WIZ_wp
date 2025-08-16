23e16b5813df8f7d89167c3cc700bc63910fea19
Atsusan <as2k.sugimoto@gmail.com>
2025-08-16 16:08:18 +0900

    [追加] コンセプトページ追加

--- page-concept.php
+++ page-concept.php
@@ -4,123 +4,146 @@ Template Name: Concept Page
 */
 ?>
 
-<?php get_header();?>
+<?php get_header(); ?>
 
-    <!-- ▼ main -->
-    <main>
-      <!-- ▼ p-conceptFv SP -->
-      <section class="p-conceptFv">
-        <div class="p-conceptFv__textContents">
-          <div class="p-conceptFv__inner">
-            <div class="p-conceptFv__textBox">
-              <h2 class="p-conceptFv__enTitle">CONCEPT</h2>
-            </div>
-          </div>
+<!-- ▼ main -->
+<main>
+  <!-- ▼ p-conceptFv SP -->
+  <section class="p-conceptFv">
+    <div class="p-conceptFv__textContents">
+      <div class="p-conceptFv__inner">
+        <div class="p-conceptFv__textBox">
+          <h2 class="p-conceptFv__enTitle">CONCEPT</h2>
         </div>
-      </section>
-      <!-- ▲ p-conceptFv SP -->
+      </div>
+    </div>
+  </section>
+  <!-- ▲ p-conceptFv SP -->
 
-      <!-- ▼ p-concept1 -->
-      <section class="p-concept1">
-        <p class="p-concept1__bigText">
-          CONCEPT<span class="p-concept1__bigNumber">1</span>
+  <!-- ▼ p-concept1 -->
+  <section class="p-concept1">
+    <p class="p-concept1__bigText">
+      CONCEPT<span class="p-concept1__bigNumber">1</span>
+    </p>
+    <div class="p-concept1__imgBox">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept1-item.jpg" alt="鮨で人を笑顔にしたい 鮨で人を繋げたい" class="p-concept1__img" width="620"
+        height="468">
+    </div>
+    <div class="p-concept1__inner">
+      <div class="p-concept1__textBox">
+        <h2 class="p-concept1__title">
+          鮨で人を笑顔にしたい
+          <br>
+          鮨で人を繋げたい
+        </h2>
+        <p class="p-concept1__text">
+          日本人はお鮨が大好きです。
+          <br>
+          お祝い事やお祭りの席で必ずお鮨は楽しまれます。
         </p>
-        <div class="p-concept1__imgBox">
-          <img src="<?php echo get_template_directory_uri();?>/assets/images/concept1-item.jpg" alt="鮨で人を笑顔にしたい 鮨で人を繋げたい" class="p-concept1__img" width="620"
-            height="468">
-        </div>
-        <div class="p-concept1__inner">
-          <div class="p-concept1__textBox">
-            <h2 class="p-concept1__title">
-              鮨で人を笑顔にしたい
-              <br>
-              鮨で人を繋げたい
-            </h2>
-            <p class="p-concept1__text">
-              日本人はお鮨が大好きです。
-              <br>
-              お祝い事やお祭りの席で必ずお鮨は楽しまれます。
-            </p>
-            <p class="p-concept1__text">
-              今の時代だからこそ、誰もがご自宅で気軽に美味しいお鮨を食べて欲しい。
-              <br>
-              鮨は日本人にとって宝であり、魔法のようなもの。
-              <br>
-              <span class="p-concept1__text--bold">
-                鮨は人を笑顔にし、人と人を繋げます。
-              </span>
-            </p>
-            <p class="p-concept1__text">
-              SUSHI WIZ
-              という名前には、どこへでも寄り添うという意味（WITH）と、寿司職人は魔法のようなお鮨と時間をお届けする魔法使い（WIZARDS）であるという意味を込めています。
-              <br>
-              大切な人と魔法のような時間を過ごして頂くため、皆様の心を幸せで満たすため、
-              <br>
-              今日もあなたのご自宅で飛びっきりのお鮨を楽しく握ります。
-            </p>
-          </div>
-        </div>
-      </section>
-      <!-- ▲ p-concept1 -->
-
-      <!-- ▼ p-concept2 -->
-      <section class="p-concept2">
-        <p class="p-concept2__bigText">
-          CONCEPT<span class="p-concept2__bigNumber">2</span>
+        <p class="p-concept1__text">
+          今の時代だからこそ、誰もがご自宅で気軽に美味しいお鮨を食べて欲しい。
+          <br>
+          鮨は日本人にとって宝であり、魔法のようなもの。
+          <br>
+          <span class="p-concept1__text--bold">
+            鮨は人を笑顔にし、人と人を繋げます。
+          </span>
         </p>
-        <div class="p-concept2__imgBox">
-          <img src="<?php echo get_template_directory_uri();?>/assets/images/concept2-item.jpg" alt="新鮮で美味しいお鮨を お手軽な価格で" class="p-concept2__img" width="546"
-            height="508">
-        </div>
-        <div class="p-concept2__inner">
-          <div class="p-concept2__textBox">
-            <h2 class="p-concept2__title">
-              新鮮で美味しいお鮨を
-              <br>
-              お手軽な価格で
-            </h2>
-            <p class="p-concept2__text">
-              新鮮で美味しいお鮨を出来る限りリーズナブルなお値段で提供するため、ご予約の当日に築地や豊洲市場で新鮮な魚を調達し、職人が仕込みます。
-              <br>
-              鮨の生命線である酢飯にもこだわり、赤酢、米酢、黒酢などを独自にブレンド、ネタに合わせた最適の味付けを追求しています。
-            </p>
-            <p class="p-concept2__text">
-              利益よりも味を追及する、をモットーに必ずひと手間を加えて、日々究極の美味しさを求め、進化する出張寿司を目指しています。
-            </p>
-          </div>
-        </div>
-      </section>
-      <!-- ▲ p-concept2 -->
-
-      <!-- ▼ p-concept3 -->
-      <section class="p-concept3">
-        <p class="p-concept3__bigText">
-          CONCEPT<span class="p-concept3__bigNumber">3</span>
+        <p class="p-concept1__text">
+          SUSHI WIZ
+          という名前には、どこへでも寄り添うという意味（WITH）と、寿司職人は魔法のようなお鮨と時間をお届けする魔法使い（WIZARDS）であるという意味を込めています。
+          <br>
+          大切な人と魔法のような時間を過ごして頂くため、皆様の心を幸せで満たすため、
+          <br>
+          今日もあなたのご自宅で飛びっきりのお鮨を楽しく握ります。
         </p>
-        <div class="p-concept3__imgBox">
-          <img src="<?php echo get_template_directory_uri();?>/assets/images/concept3-item.jpg" alt="いつでも、どこでも、どなたでも" class="p-concept3__img" width="620"
-            height="508">
-        </div>
-        <div class="p-concept3__inner">
-          <div class="p-concept3__textBox">
-            <h2 class="p-concept3__title">
-              いつでも、どこでも、
-              <br>
-              どなたでも
-            </h2>
-            <p class="p-concept3__text">
-              小さなお子様を子育て中で外食が難しい。足腰が不安で外出が出来ない。会社の大切な仲間達とお祝いパーティーを開きたいけど、人数が多くてお店に入れない。お寿司は食べたいが高級寿司はハードルが高い。それでも今日はお寿司が食べたい、というお客様の声にお応えします。
-            </p>
-            <p class="p-concept3__text">
-              お子様が大好きなツナや玉子焼き、いくらなどに特化した親子プランや、生ものが苦手な場合は火を通したお寿司メニュー、外国の方向けプラン、大人数パーティー向けプランなど、お一人おひとりのニーズに合わせながら、<span
-                class="p-concept3__text--bold">リーズナブルで美味しいお鮨をいつでも、どこでも、どなたでも楽しめるように。</span>
-            </p>
-            <p class="p-concept3__text">
-              <span class="p-concept3__text--bold">こんな出張寿司は全国どこにもありません。</span>
-            </p>
-          </div>
-        </div>
-      </section>
-      <!-- ▲ p-concept3 -->
+      </div>
+    </div>
+  </section>
+  <!-- ▲ p-concept1 -->
 
-<?php get_footer(); ?>
+  <!-- ▼ p-concept2 -->
+  <section class="p-concept2">
+    <p class="p-concept2__bigText">
+      CONCEPT<span class="p-concept2__bigNumber">2</span>
+    </p>
+    <div class="p-concept2__imgBox">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept2-item.jpg" alt="新鮮で美味しいお鮨を お手軽な価格で" class="p-concept2__img" width="546"
+        height="508">
+    </div>
+    <div class="p-concept2__inner">
+      <div class="p-concept2__textBox">
+        <h2 class="p-concept2__title">
+          新鮮で美味しいお鮨を
+          <br>
+          お手軽な価格で
+        </h2>
+        <p class="p-concept2__text">
+          新鮮で美味しいお鮨を出来る限りリーズナブルなお値段で提供するため、ご予約の当日に築地や豊洲市場で新鮮な魚を調達し、職人が仕込みます。
+          <br>
+          鮨の生命線である酢飯にもこだわり、赤酢、米酢、黒酢などを独自にブレンド、ネタに合わせた最適の味付けを追求しています。
+        </p>
+        <p class="p-concept2__text">
+          利益よりも味を追及する、をモットーに必ずひと手間を加えて、日々究極の美味しさを求め、進化する出張寿司を目指しています。
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- ▲ p-concept2 -->
+
+  <!-- ▼ p-concept3 -->
+  <section class="p-concept3">
+    <p class="p-concept3__bigText">
+      CONCEPT<span class="p-concept3__bigNumber">3</span>
+    </p>
+    <div class="p-concept3__imgBox">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept3-item.jpg" alt="いつでも、どこでも、どなたでも" class="p-concept3__img" width="620"
+        height="508">
+    </div>
+    <div class="p-concept3__inner">
+      <div class="p-concept3__textBox">
+        <h2 class="p-concept3__title">
+          いつでも、どこでも、
+          <br>
+          どなたでも
+        </h2>
+        <p class="p-concept3__text">
+          小さなお子様を子育て中で外食が難しい。足腰が不安で外出が出来ない。会社の大切な仲間達とお祝いパーティーを開きたいけど、人数が多くてお店に入れない。お寿司は食べたいが高級寿司はハードルが高い。それでも今日はお寿司が食べたい、というお客様の声にお応えします。
+        </p>
+        <p class="p-concept3__text">
+          お子様が大好きなツナや玉子焼き、いくらなどに特化した親子プランや、生ものが苦手な場合は火を通したお寿司メニュー、外国の方向けプラン、大人数パーティー向けプランなど、お一人おひとりのニーズに合わせながら、<span
+            class="p-concept3__text--bold">リーズナブルで美味しいお鮨をいつでも、どこでも、どなたでも楽しめるように。</span>
+        </p>
+        <p class="p-concept3__text">
+          <span class="p-concept3__text--bold">こんな出張寿司は全国どこにもありません。</span>
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- ▲ p-concept3 -->
+
+  <!-- ▼ p-concept4 -->
+  <section class="p-concept4">
+    <p class="p-concept4__bigText">
+      CONCEPT<span class="p-concept4__bigNumber">4</span>
+    </p>
+    <div class="p-concept4__imgBox">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept4-item.webp" alt="鮨文化の素晴らしさを伝えたい" class="p-concept4__img" width="546"
+        height="508">
+    </div>
+    <div class="p-concept4__inner">
+      <div class="p-concept4__textBox">
+        <h2 class="p-concept4__title">
+          鮨文化の素晴らしさを伝えたい
+        </h2>
+        <p class="p-concept4__text">
+          鮨の文化を少しでも皆様にお伝えするため、お子様や外国人観光客向けの「握り体験コース」をご用意致しました。新鮮な食材を自分でお鮨の形に握ってもらい、楽しく、美味しく皆様で食べましょう。<br>
+          また、お鮨に合うお酒と言えばやはり日本酒やワインです。日本酒及びワインの利き酒専門資格を取得したスタッフが厳選するお酒が、お鮨の味わいを更に深いものにしてくれます。是非お酒とのペアリングもお試しください。
+        </p>
+      </div>
+    </div>
+  </section>
+  <!-- ▲ p-concept4 -->
+
+  <?php get_footer(); ?>
